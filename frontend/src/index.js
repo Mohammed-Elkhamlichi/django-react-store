8a459dd8bb5098de927c936ed41c7af7d591859c
Mohammed-Ekhamlichi <elkhamlichimedn1et@gmail.com>
2021-12-04 11:17:20 +0100

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDom from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css.map";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+const Index = () => {
+    return (
+        <React.Fragment>
+            <div
+                style={{
+                    background: "cyan",
+                    boxShadow: "0.5px 0.5px black",
+                    margin: "10px auto",
+                    width: "500px",
+                    height: "500px",
+                }}
+            >
+                <button
+                    style={{
+                        padding: "10px 20px",
+                        backgroundColor: "green",
+                        fontWeight: "bold",
+                        color: "white",
+                        border: "none",
+                        borderRadius: "0px 20px 0px 20px",
+                    }}
+                >
+                    Dev By Mohammed Elkhamlichi
+                </button>
+            </div>
+        </React.Fragment>
+    );
+};
+
+ReactDom.render(<Index />, document.getElementById("root"));
