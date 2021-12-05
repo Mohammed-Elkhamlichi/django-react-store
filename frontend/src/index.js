9e70fcfdc34001227fb0c9757072b2dd627aed53
Mohammed-Elkhamlichi <elkhamlichimedn1et@gmail.com>
2021-12-05 19:56:07 +0100

    'update'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDom from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css.map";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Index = () => {
     return (
