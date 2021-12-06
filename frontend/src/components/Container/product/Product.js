78406e4ccc299de411f2d47f59e0bad4e3bb8981
Mohammed-Elkhamlichi <elkhamlichimedn1et@gmail.com>
2021-12-06 14:42:54 +0100

    update

--- frontend/src/components/Container/product/Product.js
+++ frontend/src/components/Container/product/Product.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
-    const { img, title, description, price } = props.product;
+    const { img, title, price } = props.product;
     return (
         <div className='product'>
             <img className='product_img' src={img} alt='iphone 13 pro max' />
