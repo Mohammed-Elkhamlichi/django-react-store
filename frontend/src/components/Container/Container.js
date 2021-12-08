7396e21e8d62d9e5cb9399c8de5647d745406a89
Mohammed-Elkhamlichi <elkhamlichimedn1et@gmail.com>
2021-12-07 19:42:51 -0800

    make update

--- frontend/src/components/Container/Container.js
+++ frontend/src/components/Container/Container.js
@@ -2,18 +2,22 @@ import React from "react";
 import Product from "./product/Product";
 import "./Container.css";
 import Products from "../data/Products";
+import { Link } from "react-router-dom";
 
 const Container = () => {
     return (
         <div className='container'>
             <div className='products'>
                 {Products.map((product) => {
-                    return <Product product={product} />;
+                    return (
+                        <Link style={{ textDecoration: "none" }} to='/product/detail'>
+                            <Product product={product} />
+                        </Link>
+                    );
                 })}
             </div>
         </div>
     );
 };
-
 
 export default Container;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,24 @@ import React from "react";
 import ReactDom from "react-dom";
 import Footer from "./components/Footer/Footer";
 import Navbar from "../src/components/Navbar/Navbar";
-import Container from '../src/components/Container/Container'
-import './Index.css'
+import Container from "../src/components/Container/Container";
+import "./Index.css";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 const Index = () => {
     return (
-        <React.Fragment>
-            <div className="index">
-                <Navbar />
-                <Container />
-                <Footer />
-            </div>
-        </React.Fragment>
+        <BrowserRouter>
+            <React.StrictMode>
+                <div className='index'>
+                    <Navbar />
+                    <Routes>
+                        <Route exact path='/' element={<Container />}></Route>
+                        <Route path='/product/detail' element={<Navbar />}></Route>
+                    </Routes>
+                    <Footer />
+                </div>
+            </React.StrictMode>
+        </BrowserRouter>
     );
 };
 
